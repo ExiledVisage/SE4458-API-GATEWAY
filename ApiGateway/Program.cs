6e46350d9214c7c8f83f310b18c0fa866219409c
ExiledVisage <kerem12irmak@hotmail.com>
2024-06-09 19:17:47 +0300

    many improvments

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,9 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
+/*builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000);  
+    options.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps(); 
+    });
+
+    ,
+  "Kestrel": {
+    "Endpoints": {
+      "Https": {
+        "Url": "https://localhost:5001"
+      }
+    }
+  }
+});*/
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireClaim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Admin"));
+});
 
 var app = builder.Build();
 
@@ -13,32 +62,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
+/*app.Use(async (context, next) => debugTool
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    if (context.User.Identity.IsAuthenticated)
+    {
+        foreach (var claim in context.User.Claims)
+        {
+            app.Logger.LogInformation($"Claim Type: {claim.Type}, Claim Value: {claim.Value}");
+        }
 
-app.Run();
+        var roleClaim = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+        var role = roleClaim?.Value;
+        app.Logger.LogInformation($"User is authenticated with role: {role}");
+    }
+    else
+    {
+        app.Logger.LogInformation("User is not authenticated");
+    }
+    await next.Invoke();
+});*/
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.MapReverseProxy();
+
+await app.RunAsync();
